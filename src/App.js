167d7d5bd43a5f89ca1ff458d1cbb16317af97e7
SatyasaiDirisala <49064005+SatyasaiDirisala@users.noreply.github.com>
2020-08-14 09:57:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div class="header">
+        <h1>My First React Website</h1>
+        <p>A website created by me.</p>
+        </div>
+        <div class="row">
+        <div class="side">
+          <h2>About Me</h2>
+          <img src="me2.png"></img>
+            <p>I'm SATYA SAI DIRISALA</p>
+          </div>
+        </div>
+    </div>
+      
+  );
+}
+
+export default App;
